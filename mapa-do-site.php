bda82e5510646f46c2c18f2a66f36ecc6630f1e8
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-01-25 11:25:35 -0300

    atualizando mapa do site

--- mapa-do-site.php
+++ mapa-do-site.php
@@ -6,22 +6,73 @@
 </head>
 
 <body>
-    Páginas do funil:
-    <br><br>
-    <a href="3-passos.php">Página de captura de contato</a>
-    <br><br>
-    <a href="obrigado.php">Página de obrigado pelo interesse</a>
-    <br><br>
-    <a href="aula.php">Página com o vídeo</a>
-    <br><br>
-    <a href="contato.php">Página com o form pós vídeo</a>
-    <br><br>
-    <a href="obrigado-pelo-contato.php">Página de obrigado pela pré-inscrição</a>
-    <br><br>
-    <a href="matricula.php">Página com o copy pós-ligação e pagamento no cartão</a>
-    <br><br>
-    <a href="matriculaBoleto.php">Página com o copy pós-ligação e mais opções de pagamento</a>
-    <br><br>
-    <a href="index.php">Página com o copy para quem chegou por fora da aula gratuita e link pro form</a>
-</body>
+
+<a href="3-passos.php">3-passos</a> - Página de caputura
+<br><br>
+<a href="obrigado.php">obrigado</a> - Agradecimento por querer ver a aula
+<br><br>
+<a href="aula.php">aula</a> - Página com a aula gratuita
+<br><br>
+<a href="contato.php">contato</a> - Formulário de pré-inscrição
+<br><br>
+<a href="ligacao.php">ligacao</a>  - Formulário de contato para ligações de pesquisa 
+<br><br>
+<a href="obrigado-pelo-contato.php">obrigado-pelo-contato</a> - Agradecimento pela pré-inscrição e pelo preenchimento para pesquisa
+<br><br>
+<a href="matricula.php">matricula</a> - Página com vídeo sobre mentoria, detalhes da mentoria e link para checkout no cartão
+<br><br>
+<a href="fazer-matricula.php">fazer-matricula</a> - Página com vídeo sobre mentoria, detalhes da mentoria e link para checkout aceitando boleto
+<br><br>
+<a href="obrigado-pela-matricula.php">obrigado-pela-matricula</a> - Agradecimento pela compra
+<br><br>
+<a href="index.php">index</a> - Página com vídeo sobre mentoria, detalhes da mentoria e link para formulário de contato
+<br><br>
+<a href="mentoria.php">mentoria</a> - Página com vídeo da aula gratuita, detalhes da mentoria e link para formulário de contato
+
+<hr>
+<h2>Páginas redirect</h2>
+
+<a href="checkout.php">checkout</a> - Redirect para Eduzz com pagamento só no cartão
+<br><br>
+<a href="fazer-checkout.php">fazer-checkout</a> - Redirect para Eduzz aceitando boleto
+<br><br>
+<a href="instagram.php">instagram</a> - Redirect perfil do Pedro no Instagram
+<br><br>
+<a href="telegram.php">telegram</a> - Redirect para canal do Telegram
+<br><br>
+<a href="depoimento-murilo.php">depoimento-murilo</a> - Redirect para Youtube com depoimento do Murilo
+<br><br>
+<a href="agende.php">agende</a> - Redirect para evento de agenda no Calendly
+<br><br>
+<a href="aula-ao-vivo.php">aula-ao-vivo</a> - Redirect para sala do Zoom
+<br><br>
+<a href="whatsapp-aula.php">whatsapp-aula</a> - Redirect para WhatsApp pedindo link da aula
+<br><br>
+<a href="whatsapp-aviso.php">whatsapp-aviso</a> - Redirect para WhatsApp pedindo para ser avisado quando tiver conteúdo novo
+<br><br>
+<a href="whatsapp-contato.php">whatsapp-contato</a> - Redirect para WhatsApp pedindo para marcar conversa
+<br><br>
+<a href="whatsapp-direto.php">whatsapp-direto</a>  - Redirect para WhatsApp sem texto padrão
+
+<hr>
+<h2>Páginas utilizadas apenas quando não estamos com campanha ativa</h2>
+
+<a href="aula-sem-campanha.php">aula-sem-campanha</a> - Aviso de aula encerrada e form para se inscrever e ser avisado no futuro
+<br><br>
+<a href="obrigado-aviso.php">obrigado-aviso</a> - Obrigado por se cadastrar sem campanha ativa
+<br><br>
+<a href="ligacao-sem-campanha.php">ligacao-sem-campanha</a> - Formulário de contato para ligações de pesquisa sem campanha ativa
+<br><br>
+<a href="contato-sem-campanha.php">contato-sem-campanha</a> - Formulário de pré-inscrição sem campanha ativa
+    
+<hr>
+<h2>Páginas sem conteúdo relevante</h2>
+
+<a href="mapa-do-site.php">mapa-do-site</a> - Essa página aqui
+<br><br>
+<a href="template-nova-pagina.php">template-nova-pagina</a> - Esqueleto para começar página nova
+<br><br>
+<a href="404.php">404</a> - Erro 404
+
+    </body>
 </html>
