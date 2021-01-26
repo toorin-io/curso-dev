d2bee6e6b7b4d097194aee27b69111f72173f8d1
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-01-26 11:52:45 -0300

    criando redirect para alura

---
+++ alura.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Conheça a formação da Alura</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://www.alura.com.br/formacao-front-end'" />
+ 
+    <?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+    
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para a Alura!</p>
+
+  <p>Caso a página não carregue automaticamente, <a
+      href="https://www.alura.com.br/formacao-front-end">clique
+      aqui para acessar</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");?> 
+
+</body>
+</html>
