be416b18c4440f808906b80b7447af6ca4ca09e6
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-02-10 18:04:55 -0300

    criando página de matrícula sem campanha

---
+++ chunks/body/cta-matricula-sem-campanha.php
@@ -0,0 +1,16 @@
+    <section class="how-to">
+            <div class="content">
+                <h2 class="how-to--title">Inscrições encerradas!</h2>
+
+                <a class="cta" href="whatsapp-proxima-turma">QUERO SABER DA PRÓXIMA TURMA</a>
+
+                <ul class="countdown">
+                    <li><span id="days">00</span> dias</li>
+                    <li><span id="hours">00</span> horas</li>
+                    <li><span id="minutes">00</span> minutos</li>
+                    <li><span id="seconds">00</span> segundos</li>
+                </ul>
+
+            </div>
+        </section>
+
---
+++ whatsapp-proxima-turma.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Solicite ser avisado pelo WhatsApp</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://api.whatsapp.com/send?phone=5521992959087&text=Ol%C3%A1%20Pedro!%20Gostaria%20de%20ser%20informado%20quando%20for%20lançada%20uma%20nova%20turma%20da%20mentoria%20Conquiste%20Sua%20Vaga!'" />
+    
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+    
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para o WhatsApp!</p>
+
+  <p>Caso a página não carregue automaticamente, <a
+      href="https://api.whatsapp.com/send?phone=5521992959087&text=Ol%C3%A1%20Pedro!%20Gostaria%20de%20ser%20informado%20quando%20for%20lançada%20uma%20nova%20turma%20da%20mentoria%20Conquiste%20Sua%20Vaga!">clique
+      aqui para me enviar a mensagem</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");
+?> 
+
+</body>
+</html>
