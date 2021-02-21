d02580adbc0aa5006e005e3fd930b0a2eda7e3b9
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-02-21 20:51:00 -0300

    trocando numero de telefone

--- whatsapp-direto.php
+++ whatsapp-direto.php
@@ -3,7 +3,7 @@
 <head>
   <title>Conquiste Sua Vaga! Contato pelo WhastApp</title>
 
-    <meta http-equiv="refresh" content="1; url = 'https://api.whatsapp.com/send?phone=5521992959087" />
+    <meta http-equiv="refresh" content="1; url = 'https://api.whatsapp.com/send?phone=5521971240906" />
 
 <?php
 include("chunks/head/google.php");
@@ -21,7 +21,7 @@ include("chunks/body/google.php");
   <div class="forward-content">
     <p>Você está sendo redirecionado para o WhatsApp!</p>
 
-    <p>Caso a página não carregue automaticamente, <a href="https://api.whatsapp.com/send?phone=5521992959087">clique aqui
+    <p>Caso a página não carregue automaticamente, <a href="https://api.whatsapp.com/send?phone=5521971240906">clique aqui
         para me enviar a mensagem</a>!</p>
   </div>
 
