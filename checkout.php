41f0e121354b2547ffa0618aefda72ddc87738ad
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-02-28 11:39:46 -0300

    atualizando mapa do site

---
+++ checkout.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Finalize sua matrícula</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://www.conquistandosuavaga.com/finalizar-matricula'" />
+ 
+    <?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+    
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+
+  <p>Caso a página não carregue automaticamente, <a
+      href="https://www.conquistandosuavaga.com/finalizar-matricula">clique
+      aqui para finalizar sua matrícula</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");?> 
+
+</body>
+</html>
