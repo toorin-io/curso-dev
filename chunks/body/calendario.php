2022b2f0d03523a7d3b288fb2e828393e08b1776
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-03-13 15:21:50 -0300

    nova campanha 04

--- chunks/body/calendario.php
+++ chunks/body/calendario.php
@@ -1,9 +1,9 @@
 <section class="calendar">
     <div class="content">
         <h2 class="calendar-title">CALENDÁRIO DAS AULAS AO VIVO</h2>
-        <p><strong>Aula 1</strong> - 13 de março - Sábado - 09:00 (horário de Brasília)</p>
-        <p><strong>Aula 2</strong> - 20 de março - Sábado - 09:00 (horário de Brasília)</p>
-        <p><strong>Aula 3</strong> - 27 de março - Sábado - 09:00 (horário de Brasília)</p>
-        <p><strong>Aula 4</strong> - 03 de abril - Sábado - 09:00 (horário de Brasília)</p>
+        <p><strong>Aula 1</strong> - 01 de abril - Quinta - 19:00 (horário de Brasília)</p>
+        <p><strong>Aula 2</strong> - 08 de abril - Quinta - 19:00 (horário de Brasília)</p>
+        <p><strong>Aula 3</strong> - 15 de abril - Quinta - 19:00 (horário de Brasília)</p>
+        <p><strong>Aula 4</strong> - 22 de abril - Quinta - 19:00 (horário de Brasília)</p>
     </div>
 </section>
