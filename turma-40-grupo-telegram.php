f4feeca63aa1126b8b3b005e2b9a0817ca1c1579
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-03-13 13:54:41 -0300

    criando pretty link para turma

---
+++ turma-40-grupo-telegram.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    
+  <title>Conquiste Sua Vaga! Acesso o grupo do Telegram!</title>
+    
+  <meta http-equiv="refresh" content="1; url = 'https://t.me/joinchat/RTRCLSxVpiMxNzQ5'" />
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+    <p>Você está sendo redirecionado para o Telegram!</p>
+
+  <p>Caso a página não carregue automaticamente, <a href="https://t.me/joinchat/RTRCLSxVpiMxNzQ5">clique aqui para acessar o grupo</a>!</p>
+</div>
+         
+<?php
+include("chunks/body/mautic-pixel.php");
+?> 
+    
+</body>
+</html>
