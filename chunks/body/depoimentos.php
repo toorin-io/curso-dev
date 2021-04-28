1b81a97524f9fba102fb430e2a0924a1d92aa7f1
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-04-28 09:18:48 -0300

    melhorias no print do depoimento

--- chunks/body/depoimentos.php
+++ chunks/body/depoimentos.php
@@ -17,8 +17,7 @@
         </div>
         <div>
             <figure class="testimonial-imagem">
-                <div class="imagem-lucas"><img src="img-linkedin-lucas.jpg" alt="Print do post do mentorado Lucas no Linkedin"></div>
-                <figcaption><a href="https://www.linkedin.com/posts/lucas-milar%C3%A9-de-oliveira-3955b6136_java-backenddeveloper-activity-6775164106790998016-IJg5">Post do Lucas no Linkedin</a></figcaption>
+                <div class="imagem-lucas"><a href="https://www.linkedin.com/posts/lucas-milar%C3%A9-de-oliveira-3955b6136_java-backenddeveloper-activity-6775164106790998016-IJg5" target="_blank"><img src="../../images/img_linkedin_lucas.jpg" alt="Print do post do mentorado Lucas no Linkedin"></a></div>
             </figure>
         </div>
     </div>
