182e746258a343c3342a70f7009a862b80ff43cf
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-08-07 09:48:49 +0100

    pequenos ajustes no footer, depoimento do davi e update pagina live

--- chunks/body/footer.php
+++ chunks/body/footer.php
@@ -1,7 +1,7 @@
     <footer class="mentoria-footer">
         <div class="content">
             <h2 class="mentoria-title">Ficou com alguma dúvida?</h2>
-            <p><a target="_blank" href="whatsapp-direto">Me mande uma mensagem no WhatsApp.</a></p>
+            <p><a target="_blank" href="whatsapp-direto">Me mande uma mensagem no WhatsApp</a></p>
             <p>Ficarei feliz de responder a todas suas perguntas!</p>
                 <ul class="contact">
                     <li><a target="_blank" href="whatsapp-direto"><img class="icon-footer" src="images/icons/whatsapp.svg" alt="WhatsApp"></a>
--- envie-seu-curriculo.php
+++ envie-seu-curriculo.php
@@ -18,39 +18,6 @@ include("chunks/head/mautic-form.php");
 ?>
 
 
-        
-<style type="text/css" scoped>
-    .mauticform_wrapper { max-width: 60%; margin: 3em 20%; color: #6f6f6f; font-weight: 400; font-family: 'Source Sans Pro', 'Helvetica Neue', 'Arial', 'sans-serif';font-size: 18px;}
-    .mauticform-innerform {}
-    .mauticform-post-success {}
-    .mauticform-name { font-weight: bold; font-size: 1.5em; margin-bottom: 3px; }
-    .mauticform-description { margin-top: 2px; margin-bottom: 10px; }
-    .mauticform-error { margin-bottom: 10px; color: red; }
-    .mauticform-message { margin-bottom: 10px;color: green; }
-    .mauticform-row { display: block; margin-bottom: 4em; }
-    .mauticform-label { font-size: 1.1em; display: block; font-weight: bold; margin-bottom: 5px; color: #333B46; }
-    .mauticform-row.mauticform-required .mauticform-label:after { color: #e32; content: " *"; display: inline; }
-    .mauticform-helpmessage { display: block; font-size: 0.9em; margin-bottom: 3px; }
-    .mauticform-errormsg { display: block; color: red; margin-top: 2px; }
-    .mauticform-selectbox, .mauticform-input, .mauticform-textarea { width: 100%; padding: 0.5em 0.5em; border: 0; border-bottom: 1px solid #333B46; background: #fff; box-sizing: border-box; }
-    .mauticform-checkboxgrp-row {}
-    .mauticform-checkboxgrp-label { font-weight: normal; }
-    .mauticform-checkboxgrp-checkbox {}
-    .mauticform-radiogrp-row {}
-    .mauticform-radiogrp-label { font-weight: normal; }
-    .mauticform-radiogrp-radio {}
-    .mauticform-button-wrapper .mauticform-button.btn-default { color: #FFFFFF;background-color: #D83A4E;border-color: none; padding: 0.6em 0; font-size: 1.2em;}
-    .mauticform-button-wrapper .mauticform-button { display: inline-block;margin-bottom: 0;font-weight: 600;text-align: center;vertical-align: middle;cursor: pointer;background-image: none;border: 1px solid transparent;white-space: nowrap;padding: 6px 12px;font-size: 18px;line-height: 1.3856;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}
-    .mauticform-button-wrapper .mauticform-button.btn-default[disabled] { background-color: #ffffff; border-color: #dddddd;}
-
-    @media only screen and (max-width: 768px) {
-        .mauticform_wrapper {
-            max-width: 80%;
-            margin: 3em 10%;
-        }
-    }
-</style>
-
     </head>
     <body>
 
@@ -62,53 +29,86 @@ include("chunks/body/google.php");
             <h1 class="big-title">Conquiste Sua Vaga!</h1>
         </header>
 
-        
-<div id="mauticform_wrapper_99curriculo" class="mauticform_wrapper">
-    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=18" id="mauticform_99curriculo" data-mautic-form="99curriculo" enctype="multipart/form-data">
-        <div class="mauticform-error" id="mauticform_99curriculo_error"></div>
-        <div class="mauticform-message" id="mauticform_99curriculo_message"></div>
+        <h2>Participe da Live onde eu analisarei perfis no LinkedIn ao vivo, quarta-feira às 19 horas!</h2>
+
+<style type="text/css" scoped>
+    .mauticform_wrapper { max-width: 600px; margin: 10px auto; }
+    .mauticform-innerform {}
+    .mauticform-post-success {}
+    .mauticform-name { font-weight: bold; font-size: 1.5em; margin-bottom: 3px; }
+    .mauticform-description { margin-top: 2px; margin-bottom: 10px; }
+    .mauticform-error { margin-bottom: 10px; color: red; }
+    .mauticform-message { margin-bottom: 10px;color: green; }
+    .mauticform-row { display: block; margin-bottom: 20px; }
+    .mauticform-label { font-size: 1.1em; display: block; font-weight: bold; margin-bottom: 5px; }
+    .mauticform-row.mauticform-required .mauticform-label:after { color: #e32; content: " *"; display: inline; }
+    .mauticform-helpmessage { display: block; font-size: 0.9em; margin-bottom: 3px; }
+    .mauticform-errormsg { display: block; color: red; margin-top: 2px; }
+    .mauticform-selectbox, .mauticform-input, .mauticform-textarea { width: 100%; padding: 0.5em 0.5em; border: 1px solid #CCC; background: #fff; box-shadow: 0px 0px 0px #fff inset; border-radius: 4px; box-sizing: border-box; }
+    .mauticform-checkboxgrp-row {}
+    .mauticform-checkboxgrp-label { font-weight: normal; }
+    .mauticform-checkboxgrp-checkbox {}
+    .mauticform-radiogrp-row {}
+    .mauticform-radiogrp-label { font-weight: normal; }
+    .mauticform-radiogrp-radio {}
+    .mauticform-button-wrapper .mauticform-button.btn-default, .mauticform-pagebreak-wrapper .mauticform-pagebreak.btn-default { color: #5d6c7c;background-color: #ffffff;border-color: #dddddd;}
+    .mauticform-button-wrapper .mauticform-button, .mauticform-pagebreak-wrapper .mauticform-pagebreak { display: inline-block;margin-bottom: 0;font-weight: 600;text-align: center;vertical-align: middle;cursor: pointer;background-image: none;border: 1px solid transparent;white-space: nowrap;padding: 6px 12px;font-size: 13px;line-height: 1.3856;border-radius: 3px;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}
+    .mauticform-button-wrapper .mauticform-button.btn-default[disabled], .mauticform-pagebreak-wrapper .mauticform-pagebreak.btn-default[disabled] { background-color: #ffffff; border-color: #dddddd; opacity: 0.75; cursor: not-allowed; }
+    .mauticform-pagebreak-wrapper .mauticform-button-wrapper {  display: inline; }
+</style>
+<div id="mauticform_wrapper_08curriculo" class="mauticform_wrapper">
+    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=25" id="mauticform_08curriculo" data-mautic-form="08curriculo" enctype="multipart/form-data">
+        <div class="mauticform-error" id="mauticform_08curriculo_error"></div>
+        <div class="mauticform-message" id="mauticform_08curriculo_message"></div>
         <div class="mauticform-innerform">
 
             
           <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
 
-            <div id="mauticform_99curriculo_nome" class="mauticform-row mauticform-text mauticform-field-1">
-                <label id="mauticform_label_99curriculo_nome" for="mauticform_input_99curriculo_nome" class="mauticform-label">Nome</label>
-                <input id="mauticform_input_99curriculo_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
+            <div id="mauticform_08curriculo_nome" data-validate="nome" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
+                <label id="mauticform_label_08curriculo_nome" for="mauticform_input_08curriculo_nome" class="mauticform-label">Nome</label>
+                <input id="mauticform_input_08curriculo_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;">Isso é obrigatório.</span>
+            </div>
+
+            <div id="mauticform_08curriculo_email" data-validate="email" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-2 mauticform-required">
+                <label id="mauticform_label_08curriculo_email" for="mauticform_input_08curriculo_email" class="mauticform-label">E-mail</label>
+                <input id="mauticform_input_08curriculo_email" name="mauticform[email]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;">Isso é obrigatório.</span>
+            </div>
+
+            <div id="mauticform_08curriculo_telefone" class="mauticform-row mauticform-text mauticform-field-3">
+                <label id="mauticform_label_08curriculo_telefone" for="mauticform_input_08curriculo_telefone" class="mauticform-label">Número de WhatsApp</label>
+                <input id="mauticform_input_08curriculo_telefone" name="mauticform[telefone]" value="" class="mauticform-input" type="text">
                 <span class="mauticform-errormsg" style="display: none;"></span>
             </div>
 
-            <div id="mauticform_99curriculo_email" class="mauticform-row mauticform-text mauticform-field-2">
-                <label id="mauticform_label_99curriculo_email" for="mauticform_input_99curriculo_email" class="mauticform-label">E-mail</label>
-                <input id="mauticform_input_99curriculo_email" name="mauticform[email]" value="" class="mauticform-input" type="text">
+            <div id="mauticform_08curriculo_link_para_seu_linkedin" data-validate="link_para_seu_linkedin" data-validation-type="url" class="mauticform-row mauticform-url mauticform-field-4 mauticform-required">
+                <label id="mauticform_label_08curriculo_link_para_seu_linkedin" for="mauticform_input_08curriculo_link_para_seu_linkedin" class="mauticform-label">Link para seu LinkedIn</label>
+                <input id="mauticform_input_08curriculo_link_para_seu_linkedin" name="mauticform[link_para_seu_linkedin]" value="" class="mauticform-input" type="url">
+                <span class="mauticform-errormsg" style="display: none;">Isso é obrigatório.</span>
+            </div>
+
+            <div id="mauticform_08curriculo_gostaria_de_comentar_algu" class="mauticform-row mauticform-text mauticform-field-5">
+                <label id="mauticform_label_08curriculo_gostaria_de_comentar_algu" for="mauticform_input_08curriculo_gostaria_de_comentar_algu" class="mauticform-label">Gostaria de comentar alguma coisa comigo antes da live?</label>
+                <input id="mauticform_input_08curriculo_gostaria_de_comentar_algu" name="mauticform[gostaria_de_comentar_algu]" value="" class="mauticform-input" type="text">
                 <span class="mauticform-errormsg" style="display: none;"></span>
             </div>
 
-            <div id="mauticform_99curriculo_telefone" class="mauticform-row mauticform-text mauticform-field-3">
-                <label id="mauticform_label_99curriculo_telefone" for="mauticform_input_99curriculo_telefone" class="mauticform-label">Telefone</label>
-                <input id="mauticform_input_99curriculo_telefone" name="mauticform[telefone]" value="" class="mauticform-input" type="text">
-                <span class="mauticform-errormsg" style="display: none;"></span>
-            </div>
-
-            <div id="mauticform_99curriculo_link_para_seu_curriculo" class="mauticform-row mauticform-url mauticform-field-4">
-                <label id="mauticform_label_99curriculo_link_para_seu_curriculo" for="mauticform_input_99curriculo_link_para_seu_curriculo" class="mauticform-label">Link para seu currículo</label>
-                <input id="mauticform_input_99curriculo_link_para_seu_curriculo" name="mauticform[link_para_seu_curriculo]" value="" class="mauticform-input" type="url">
-                <span class="mauticform-errormsg" style="display: none;"></span>
-            </div>
-
-            <div id="mauticform_99curriculo_submit" class="mauticform-row mauticform-button-wrapper mauticform-field-5">
-                <button type="submit" name="mauticform[submit]" id="mauticform_input_99curriculo_submit" value="" class="mauticform-button btn btn-default">Enviar</button>
+            <div id="mauticform_08curriculo_submit" class="mauticform-row mauticform-button-wrapper mauticform-field-6">
+                <button type="submit" name="mauticform[submit]" id="mauticform_input_08curriculo_submit" value="" class="mauticform-button btn btn-default">Enviar</button>
             </div>
             </div>
         </div>
 
-        <input type="hidden" name="mauticform[formId]" id="mauticform_99curriculo_id" value="18">
-        <input type="hidden" name="mauticform[return]" id="mauticform_99curriculo_return" value="">
-        <input type="hidden" name="mauticform[formName]" id="mauticform_99curriculo_name" value="99curriculo">
+        <input type="hidden" name="mauticform[formId]" id="mauticform_08curriculo_id" value="25">
+        <input type="hidden" name="mauticform[return]" id="mauticform_08curriculo_return" value="">
+        <input type="hidden" name="mauticform[formName]" id="mauticform_08curriculo_name" value="08curriculo">
 
         </form>
 </div>
-      
+
+
 
 <?php
 include("chunks/body/top-stripe.php");
