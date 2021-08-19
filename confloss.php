a15b5912cd7b41b679978f2a4e60c62584b334a9
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-08-19 16:54:57 +0100

    criar páginas para cofloss

---
+++ confloss.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+
+        
+        <title>Conquiste Sua Vaga - Receba a aula</title>
+
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta-mentoria.php");
+?>
+        
+            
+<?php
+include("chunks/head/mautic-form.php");
+?>
+
+
+    </head>
+    <body>
+
+<?php
+include("chunks/body/google.php");
+?>
+
+        <header class="content header">
+            <h1 class="big-title">Conquiste Sua Vaga!</h1>
+        </header>
+
+        <h2>Obrigado por ter participado de nossa palestra no CONFLOSS!</h2>
+
+        <p>
+        Preencha o formulário abaixo para receber em primeira mão a aula "Os sete erros que estão te impedindo de conquistar sua vaga dev"!
+        </p>
+        
+
+        <style type="text/css" scoped>
+    .mauticform_wrapper { max-width: 600px; margin: 10px auto; color: #6f6f6f; font-weight: 400; font-family: 'Source Sans Pro', 'Helvetica Neue', 'Arial', 'sans-serif';font-size: 18px;}
+    .mauticform-innerform {}
+    .mauticform-post-success {}
+    .mauticform-name { font-weight: bold; font-size: 1.5em; margin-bottom: 3px; }
+    .mauticform-description { margin-top: 2px; margin-bottom: 10px; }
+    .mauticform-error { margin-bottom: 10px; color: red; }
+    .mauticform-message { margin-bottom: 10px;color: green; }
+    .mauticform-row { display: block; margin-bottom: 20px; }
+    .mauticform-label { font-size: 1.1em; display: block; font-weight: bold; margin-bottom: 5px; color: #21c5ba; }
+    .mauticform-row.mauticform-required .mauticform-label:after { color: #e32; content: " *"; display: inline; }
+    .mauticform-helpmessage { display: block; font-size: 0.9em; margin-bottom: 3px; }
+    .mauticform-errormsg { display: block; color: red; margin-top: 2px; }
+    .mauticform-selectbox, .mauticform-input, .mauticform-textarea { width: 100%; padding: 0.5em 0.5em; border: 0; border-bottom: 1px solid #21c5ba; background: #fff; box-sizing: border-box; }
+    .mauticform-checkboxgrp-row {}
+    .mauticform-checkboxgrp-label { font-weight: normal; }
+    .mauticform-checkboxgrp-checkbox {}
+    .mauticform-radiogrp-row {}
+    .mauticform-radiogrp-label { font-weight: normal; }
+    .mauticform-radiogrp-radio {}
+    .mauticform-button-wrapper .mauticform-button.btn-default { color: #21c5ba;background-color: #ffffff;border-color: #21c5ba;}
+    .mauticform-button-wrapper .mauticform-button { display: inline-block;margin-bottom: 0;font-weight: 600;text-align: center;vertical-align: middle;cursor: pointer;background-image: none;border: 1px solid transparent;white-space: nowrap;padding: 6px 12px;font-size: 18px;line-height: 1.3856;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}
+    .mauticform-button-wrapper .mauticform-button.btn-default[disabled] { background-color: #ffffff; border-color: #dddddd;}
+</style>
+
+<div id="mauticform_wrapper_99palestraconfloss" class="mauticform_wrapper">
+    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=26" id="mauticform_99palestraconfloss" data-mautic-form="99palestraconfloss" enctype="multipart/form-data">
+        <div class="mauticform-error" id="mauticform_99palestraconfloss_error"></div>
+        <div class="mauticform-message" id="mauticform_99palestraconfloss_message"></div>
+        <div class="mauticform-innerform">
+
+            
+          <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
+
+            <div id="mauticform_99palestraconfloss_nome" data-validate="nome" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
+                <label id="mauticform_label_99palestraconfloss_nome" for="mauticform_input_99palestraconfloss_nome" class="mauticform-label">Nome</label>
+                <input id="mauticform_input_99palestraconfloss_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;">Por favor, informe seu nome</span>
+            </div>
+
+            <div id="mauticform_99palestraconfloss_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-2 mauticform-required">
+                <label id="mauticform_label_99palestraconfloss_email" for="mauticform_input_99palestraconfloss_email" class="mauticform-label">E-mail</label>
+                <span class="mauticform-helpmessage">Digite aqui seu melhor e-mail</span>
+                <input id="mauticform_input_99palestraconfloss_email" name="mauticform[email]" value="" class="mauticform-input" type="email">
+                <span class="mauticform-errormsg" style="display: none;">Por favor, informe seu melhor e-mail</span>
+            </div>
+
+            <div id="mauticform_99palestraconfloss_numero_de_whatsapp" class="mauticform-row mauticform-text mauticform-field-3">
+                <label id="mauticform_label_99palestraconfloss_numero_de_whatsapp" for="mauticform_input_99palestraconfloss_numero_de_whatsapp" class="mauticform-label">Número de WhatsApp</label>
+                <span class="mauticform-helpmessage">Lembre-se de colocar o DDD!</span>
+                <input id="mauticform_input_99palestraconfloss_numero_de_whatsapp" name="mauticform[numero_de_whatsapp]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;"></span>
+            </div>
+
+            <div id="mauticform_99palestraconfloss_quero_assistir_a_aula" class="mauticform-row mauticform-button-wrapper mauticform-field-4">
+                <button type="submit" name="mauticform[quero_assistir_a_aula]" id="mauticform_input_99palestraconfloss_quero_assistir_a_aula" value="" class="mauticform-button btn btn-default">Quero receber o link para a aula!</button>
+            </div>
+            </div>
+        </div>
+
+        <input type="hidden" name="mauticform[formId]" id="mauticform_99palestraconfloss_id" value="26">
+        <input type="hidden" name="mauticform[return]" id="mauticform_99palestraconfloss_return" value="">
+        <input type="hidden" name="mauticform[formName]" id="mauticform_99palestraconfloss_name" value="99palestraconfloss">
+
+        </form>
+</div>
+
+
+
+<?php
+include("chunks/body/top-stripe.php");
+include("chunks/body/bottom-stripe.php");
+include("chunks/body/mautic-pixel.php");
+?> 
+    
+        
+        </body>
+</html>
