9715f050ffe2fd6846fd019456310f5253f0d26e
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-08-27 17:06:38 +0100

    virando página de obrigado

--- obrigado-1-passo.php
+++ obrigado-1-passo.php
@@ -20,36 +20,22 @@ include("chunks/body/google.php");
 ?>
 
 	<header class="content header">
-		<h1>Siga os 3 passos abaixo para garantir que você terá acesso à sua aula!</h1>
+		<h1>Só falta mais 1 passo para garantir que você terá acesso à sua aula!</h1>
 	</header>
 
 	<section class="content">
 		<div class="video">
-			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/T--14AvTvmY?rel=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/CMgASk4uF_g?rel=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 	</section>
 
 	<section class="selling-points">
 		<div class="content">
 			<p class="selling-text">
-				<strong>Passo 1</strong>
+				<strong>Próximo Passo</strong>
 				Peça a aula pelo WhatsApp!
 				<span>
 					<a target="_blank" href="whatsapp-aula"><img class="selling-icon" src="images/icons/whatsapp.png" alt=""></a>
-				</span>
-			</p>
-			<p class="selling-text">
-				<strong>Passo 2</strong>
-				Entre no canal do Telegram!
-				<span>
-					<a target="_blank" href="telegram"><img class="selling-icon" src="images/icons/telegram.png" alt=""></a>
-				</span>
-			</p>			
-            <p class="selling-text">
-				<strong>Passo 3</strong>
-				Me siga lá no Instagram!
-				<span>
-					<a target="_blank" href="instagram"><img class="selling-icon" src="images/icons/instagram.png" alt=""></a>
 				</span>
 			</p>
 		</div>
