c72e84e46b8376d94e59a6b85a8007cd88a44951
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-08-27 10:16:21 +0100

    adicionando nome no form de captacao

--- chunks/body/mautic-form-aula-campanha.php
+++ chunks/body/mautic-form-aula-campanha.php
@@ -1,3 +1,4 @@
+
 <div id="mauticform_wrapper_09aulagratuita" class="mauticform_wrapper">
     <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=28" id="mauticform_09aulagratuita" data-mautic-form="09aulagratuita" enctype="multipart/form-data">
         <div class="mauticform-error" id="mauticform_09aulagratuita_error"></div>
@@ -7,14 +8,27 @@
             
           <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
 
-            <div id="mauticform_09aulagratuita_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-1 mauticform-required">
+            <div id="mauticform_09aulagratuita_nome" data-validate="nome" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
+                <label id="mauticform_label_09aulagratuita_nome" for="mauticform_input_09aulagratuita_nome" class="mauticform-label">Nome</label>
+                <input id="mauticform_input_09aulagratuita_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;">Isso é obrigatório.</span>
+            </div>
+
+            <div id="mauticform_09aulagratuita_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-2 mauticform-required">
                 <label id="mauticform_label_09aulagratuita_email" for="mauticform_input_09aulagratuita_email" class="mauticform-label">E-mail</label>
                 <span class="mauticform-helpmessage">Digite aqui seu melhor e-mail</span>
                 <input id="mauticform_input_09aulagratuita_email" name="mauticform[email]" value="" class="mauticform-input" type="email">
                 <span class="mauticform-errormsg" style="display: none;">Por favor, informe seu melhor e-mail</span>
             </div>
 
-            <div id="mauticform_09aulagratuita_quero_assistir_a_aula" class="mauticform-row mauticform-button-wrapper mauticform-field-2">
+            <div id="mauticform_09aulagratuita_numero_de_whatsapp" class="mauticform-row mauticform-text mauticform-field-3">
+                <label id="mauticform_label_09aulagratuita_numero_de_whatsapp" for="mauticform_input_09aulagratuita_numero_de_whatsapp" class="mauticform-label">Número de WhatsApp</label>
+                <span class="mauticform-helpmessage">Lembre-se de colocar o DDD!</span>
+                <input id="mauticform_input_09aulagratuita_numero_de_whatsapp" name="mauticform[numero_de_whatsapp]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;"></span>
+            </div>
+
+            <div id="mauticform_09aulagratuita_quero_assistir_a_aula" class="mauticform-row mauticform-button-wrapper mauticform-field-4">
                 <button type="submit" name="mauticform[quero_assistir_a_aula]" id="mauticform_input_09aulagratuita_quero_assistir_a_aula" value="" class="mauticform-button btn btn-default">Quero descobrir agora!</button>
             </div>
             </div>
