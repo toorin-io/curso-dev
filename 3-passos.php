6d3b120494ec8f212eebed2ba6a2442601b54c0a
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-09-08 14:00:47 +0100

    landing page

--- 3-passos.php
+++ 3-passos.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html lang="pt-BR">
 <head>
 
@@ -30,7 +30,7 @@ include("chunks/body/google.php");
 		
         
         <div class="presentation" id="form_header">
-		<h2 id="form-title" class="presentation-title">Inscreva-se agora gratuitamente para assistir à aula!</h2>      
+		<h2 id="form-title" class="presentation-title">Inscreva-se agora para assistir à aula gratuita!</h2>      
 <?php
 include("chunks/body/mautic-form-aula-campanha.php");
 ?>
@@ -65,24 +65,20 @@ include("chunks/body/mautic-form-aula-campanha.php");
 	</section>
 
 	<section class="content description">
-		<h2 class="description-title">Para ser contratado é preciso ter experiência, e para ter experiência é preciso já ter trabalhado?</h2>
-        <p>Você estuda pra caramba, faz cursos, já sabe resolver problemas com lógica de programação, e sonha em viver desenvolvendo profissionalmente soluções para <strong>problemas reais.</strong></p>
-        <p> Mas, <strong>existe um problema no seu caminho que parece insuperável:</strong> os recrutadores dizem que você precisa ter 2 anos de experiência em qualquer tecnologia para conquistar sua primeira vaga.</p>
-        <p>Essa situação te deixa frustado. Com vontade de desistir. E eu te entendo. Quando eu comecei, <strong>todas as vagas pareciam inatingíveis para minha experiência.</strong></p>
-        <p>Sempre eram <strong>pedidos anos de conhecimento em diversas tecnologias!</strong> E eu pensava: "Como afinal um júnior, que está começando agora, pode saber tudo isso???"</p>
-        <p>Não foi fácil manter a cabeça de pé! Tiveram horas que quase achei que a carreira de dev não fosse para mim. Mas <strong>eu não desisti</strong>.</p>
-        <p>Eu tinha <strong>o sonho de conseguir a minha vaga de dev</strong>, ganhando um ótimo salário numa área que só cresce e nunca entra em crise, e por com isso dar à minha esposa e meu filho a vida que eles merecem.</p>
-        <p>Com esse futuro melhor em mente, procurando alguém disposto a me dar minha primeira oportunidade na area, eu <strong>enviei centenas de currículos e não recebi nenhuma resposta!</strong></p>
-        <p>Até que eu entendi que <strong>não adianta continuar fazendo a mesma coisa</strong> que não está dando certo torcendo para o resultado ser diferente. Entendi que não seria dessa forma, me comportando como uma metralhadora de currículos, que alguém iria me dar minha primeira oportunidade como dev.</p> 
-        <p>Foi então que eu mudei a minha estratégia, e aprendi <strong>como mostrar meu valor para os recrutadores</strong>, independente dos anos de experiência!</p>
-        <p>Com essa estratégia, eu não só conquistei minha primeira vaga como depois nunca mais parei. <strong>Oportunidades melhores nunca parar de bater na minha porta</strong>, e hoje já tenho mais de 16 anos atuando com desenvolvimento.</p>
-        <p>Crescendo na carreira eu passei a gerenciar equipes e ser o responsável pela <strong>contratação de devs</strong> para os meus times.</p>
-        <p>Após entrevistar milhares de pessoas, eu percebi exatamente o que leva um recrutador a eliminar um candidato imediatamente, e descobri que <strong>existem técnicas que qualquer um pode seguir para se tornar o candidato dos sonhos.</strong></p>
-        <p>Hoje eu juntei essas técnicas, que ajudaram tanto a mim como a diversos outros profissionais a vencerem na carreira, e as compartilhei em uma aula, que está <strong>disponível agora para você.</strong></p>
-        <p>É uma aula onde eu te conto o que a faculdade não te ensina. Te explico o que você precisa fazer para se tornar o <strong>um dev desejado pelo mercado.</strong></p>
-        <p>Nesta aula compartilho as três técnicas farão você dominar o mercado e <strong>se colocar entre os profissionais mais cobiçados pelos recrutadores.</strong> Entre aqueles que constroem uma carreira escolhendo onde querem trabalhar e quanto querem ganhar!</p>
+		<h2 class="description-title">O que você vai aprender na aula gratuita?</h2>
+
+        <p><strong>Para ser contratado é preciso ter experiência, e para ter experiência é preciso já ter trabalhado!</strong></p>
+        <p>Você estuda, faz cursos livres na internet, às vezes até mesmo uma faculdade. Já sabe resolver problemas de programação, e sonha em ganhar a vida desenvolvendo soluções para problemas reais. Mas… a sua falta de experiência que não permite ter a primeira oportunidade <strong>parece um problema sem solução.</strong></p>
+        <p>Quando comecei, <strong>todas as vagas pareciam inatingíveis para minha experiência.</strong> Sempre eram pedidos anos de conhecimento em muitas tecnologias. Como afinal um júnior, que está começando agora, pode saber tudo isso???</p>
+        <p>Não foi fácil! Tiveram horas que quase achei que a carreira de dev não fosse para mim. Mas <strong>eu não desisti</strong>. Eu tinha o sonho de conseguir uma vaga como dev, com um bom salário na área que nunca tem crise, e dar à minha família a vida que ela merece.</p>
+        <p>Nas primeiras vezes que tentei conquistar uma vaga, <strong>enviei centenas de currículos e não recebi nenhuma resposta!</strong></p>
+        <p>Levou muita tentativa e erro, mas eu consegui <strong>aprender como mostrar meu valor para os recrutadores.</strong> Hoje, já estou há mais de 15 anos atuando como dev.</p>
+        <p>Crescendo na carreira passei a ser também responsável pela contratação de diversos deves para o meu time. Após entrevistar milhares de pessoas, descobri que <strong>existem técnicas que qualquer um pode seguir para se tornar o candidato dos sonhos.</strong></p>
+        <p>Hoje eu juntei essas técnicas, que ajudaram tanto a mim como a diversos outros profissionais a terem sucesso, e as compartilhei em uma aula, que está <strong>disponível agora para você.</strong></p>
+        <p>É uma aula onde eu te conto o que a faculdade não ensina. Te explico o que fazer para se transformar em um dev desejado pelo mercado.</p>
+        <p>Nesta aula compartilho as três técnicas farão você <strong>se colocar entre os profissionais mais cobiçados pelos recrutadores.</strong> Entre aqueles que constroem uma carreira escolhendo onde querem trabalhar e quanto querem ganhar!</p>
         <p><strong>⚠️ Não perca tempo! A aula ficará disponível somente até o dia 12 de setembro.</strong></p>
-        <p>Se inscreva agora para assistir à aula <strong>sem nenhum custo!</strong> Eu garanto que ao final você saberá exatamente quais passos dar para conquistar sua vaga!</p>
+        <p><strong>Preencha o formulário</strong> e se inscreva para assistir à aula sem nenhum custo! Ao final dela você saberá exatamente quais passos dar para conquistar sua vaga!</p>
 
 		<a class="action-button" href="#form-title">Quero assistir à aula!</a>
 	</section>
@@ -90,8 +86,8 @@ include("chunks/body/mautic-form-aula-campanha.php");
 	<section class="content description bio">
 		<img class="bio-photo" src="./images/pedromarins.jpg" alt="Pedro Marins">
 		<h2 class="description-title">Pedro Marins</h2>
-		<p>Eu sou o Pedro Marins. Minha missão de vida é ajudar quem deseja começar na carreira de dev. Comecei a trabalhar como dev há 16 anos e hoje também atuo como professor na Alura e no Ibmec. Sou MBA em Gestão de Conhecimento e Negócios pela COPPE/UFRJ, e tenho minha empresa de desenvolvimento de software, onde sou criador de negócios inovadores.</p>
-		<p>Depois de trabalhar em uma centena de projetos, montar dezenas de equipes e entrevistar mais de mil pessoas, chegou a hora de compartilhar o método que utilizei para construir minha carreira e ajudar você a também conquistar sua vaga!</p>
+		<p>Sou o Pedro Marins. Minha missão é ajudar quem deseja começar na carreira de dev. Comecei a trabalhar como dev há 15 anos e hoje também atuo como professor na Alura e no Ibmec. Sou MBA em Gestão de Conhecimento e Negócios pela COPPE/UFRJ, e tenho minha empresa de desenvolvimento de software, onde sou criador de negócios inovadores.</p>
+		<p>Depois de trabalhar em uma centena de projetos, montar dezenas de equipes e entrevistar mais de mil pessoas, chegou a hora de mostrar como eu construí minha carreira e ajudar você a conquistar sua vaga também!</p>
         
 		<a class="action-button" href="#form-title">Quero assistir à aula!</a>
 	</section>
