7cf9893757b518d6a876d2b7d722296f4c0d846f
Pedro Marins <contato@pedromarins.com>
2021-09-09 09:28:55 -0300

    primeira versão da página de histórias

---
+++ historias.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+      
+    <title>Conquiste Sua Vaga! Conheça a mentoria</title>
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta-mentoria.php");
+?>
+    
+</head>
+
+<body>
+
+<?php
+include("chunks/body/google.php");
+?>
+
+    <div class="top-warning">
+        <p>Como é a experiência de participar da mentoria Conquista Sua Vaga?</p>
+    </div>
+
+    
+<main>
+
+<ul class="stories">
+    <li class="story">
+        <div class="story-division">
+            <figure class="story-author">
+                <img src="images/historias/renato-lewkowicz.jpg" alt="Renato Lewkowicz" class="story-author-photo">
+                <figcaption class="story-author-details">
+                    <p class="story-author-name">Renato Lewkowicz</p>
+                    <a href="https://www.linkedin.com/in/renatolewkowicz/" class="story-author-linkedin">
+                        <img src="images/icons/linkedin.svg" alt="LinkedIn">
+                    </a>
+                </figcaption>
+            </figure>
+
+            <div class="story-content">
+                <h3 class="story-content-cite">"Hoje é o meu 29º dia de mentoria e amanhã eu começo na minha primeira vaga."</h3>
+                <div class="story-content-video">
+                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/M7hOIhpy7us" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <p class="story-content-text">"Eu não sabia nada para ser sincero. Eu tinha um conhecimento básico em HTML e CSS. Já tinha feito alguns projetinhos em Python, mas coisas muito básicas... Estipulei que em até 6 meses eu estaria confiante para a minha primeira vaga e estar confiante para entrar no mercado como desenvolvedor... A minha primeira proposta, o meu primeiro sim, veio com 24 dias de mentoria."</p>
+            </div>
+        </div>
+    </li>
+</ul>
+
+
+<?php
+include("chunks/body/depoimentos.php");
+?>
+
+<?php
+include("chunks/body/faq.php");
+include("chunks/body/sale-box.php");
+include("chunks/body/cta-matricula-cartao.php");
+?>
+         
+</main>
+
+<?php
+include("chunks/body/footer.php");
+include("chunks/body/bottom-stripe.php");
+include("chunks/body/mautic-pixel.php");
+?> 
+        
+</body>
+
+</html>
+
+<!-- 463 -->
