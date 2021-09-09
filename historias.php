4997d6af44ae2b4e726f93408a85bb6bbc5cc315
Pedro Marins <contato@pedromarins.com>
2021-09-09 17:24:18 -0300

    adicionando mais duas histórias

--- historias.php
+++ historias.php
@@ -17,17 +17,13 @@ include("chunks/head/meta-mentoria.php");
 
 <?php
 include("chunks/body/google.php");
-?>
-
-    <div class="top-warning">
-        <p>Como é a experiência de participar da mentoria Conquista Sua Vaga?</p>
-    </div>
-
-    
+?>    
 <main>
 
+
+
 <ul class="stories">
-    <li class="story">
+    <li class="story" id="renato-lewkowicz">
         <div class="story-division">
             <figure class="story-author">
                 <img src="images/historias/renato-lewkowicz.jpg" alt="Renato Lewkowicz" class="story-author-photo">
@@ -40,20 +36,59 @@ include("chunks/body/google.php");
             </figure>
 
             <div class="story-content">
-                <h3 class="story-content-cite">"Hoje é o meu 29º dia de mentoria e amanhã eu começo na minha primeira vaga."</h3>
+                <h3 class="story-content-cite">"Hoje é o meu 29º dia de mentoria e amanhã eu começo na minha primeira vaga!"</h3>
                 <div class="story-content-video">
                     <iframe width="100%" height="400" src="https://www.youtube.com/embed/M7hOIhpy7us" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
-                <p class="story-content-text">"Eu não sabia nada para ser sincero. Eu tinha um conhecimento básico em HTML e CSS. Já tinha feito alguns projetinhos em Python, mas coisas muito básicas... Estipulei que em até 6 meses eu estaria confiante para a minha primeira vaga e estar confiante para entrar no mercado como desenvolvedor... A minha primeira proposta, o meu primeiro sim, veio com 24 dias de mentoria."</p>
+                <p class="story-content-text">"<strong>Eu não sabia nada para ser sincero</strong>. Eu tinha um conhecimento básico em HTML e CSS. Já tinha feito alguns projetinhos em Python, mas coisas muito básicas... Estipulei que em até 6 meses <strong>eu estaria confiante para a minha primeira vaga</strong> e estar confiante para entrar no mercado como desenvolvedor... A minha primeira proposta, o meu primeiro sim, veio com 24 dias de mentoria."</p>
+            </div>
+        </div>
+    </li>
+
+    <li class="story" id="felipe-marcondes">
+        <div class="story-division">
+            <figure class="story-author">
+                <img src="images/historias/felipe-marcondes.jpg" alt="Felipe Marcondes" class="story-author-photo">
+                <figcaption class="story-author-details">
+                    <p class="story-author-name">Felipe Marcondes</p>
+                    <a href="https://www.linkedin.com/in/felipemgc/" class="story-author-linkedin">
+                        <img src="images/icons/linkedin.svg" alt="LinkedIn">
+                    </a>
+                </figcaption>
+            </figure>
+
+            <div class="story-content">
+                <h3 class="story-content-cite">"Com uma semana e meia de mentoria eu já tive contato da primeira recrutadora!"</h3>
+                <div class="story-content-video">
+                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/6tYsAvUsQSY " title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <p class="story-content-text">"Conteúdo preto no branco. Muito sincero. Direto ao ponto... Eu decidi apostar na mentoria e não me arrependi nem um pouco! Em um mês e pouquinho de que eu comecei a mentoria eu consegui esse emprego de Desenvolvedor Front-end Junior! O fato é que <strong>todas as pessoas que eu percebi que participaram da mentoria em algum momento conseguiram a sua vaga</strong>. É uma aposta certíssima a se fazer."</p>
+            </div>
+        </div>
+    </li>
+
+    <li class="story" id="lucas-milare">
+        <div class="story-division">
+            <figure class="story-author">
+                <img src="images/historias/lucas-milare.jpg" alt="Felipe Marcondes" class="story-author-photo">
+                <figcaption class="story-author-details">
+                    <p class="story-author-name">Lucas Milaré</p>
+                    <a href="https://www.linkedin.com/in/lucas-milar%C3%A9-de-oliveira-3955b6136/" class="story-author-linkedin">
+                        <img src="images/icons/linkedin.svg" alt="LinkedIn">
+                    </a>
+                </figcaption>
+            </figure>
+
+            <div class="story-content">
+                <h3 class="story-content-cite">"Eu fui aprovado e consegui a minha tão sonhada vaga como desenvolvedor junior!"</h3>
+                <div class="story-content-video">
+                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/JMJFzBvT1FU  " title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <p class="story-content-text">"<strong>A primeira oportunidade é muito difícil quando você não tem conhecimento do que fazer</strong>. Participar da mentoria foi uma das melhores escolhas que eu pude fazer. O que é abordado na mentoria é o que cai nas entrevistas. Fui abordado por uma recrutadora me propondo para participar de uma vaga! <strong>Para quem ainda tem dúvida: esse negócio realmente funciona!</strong>"</p>
             </div>
         </div>
     </li>
 </ul>
-
-
-<?php
-include("chunks/body/depoimentos.php");
-?>
 
 <?php
 include("chunks/body/faq.php");
@@ -65,6 +100,7 @@ include("chunks/body/cta-matricula-cartao.php");
 
 <?php
 include("chunks/body/footer.php");
+include("chunks/body/top-stripe.php");
 include("chunks/body/bottom-stripe.php");
 include("chunks/body/mautic-pixel.php");
 ?> 
