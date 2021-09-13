da7cd4c564c206a36ff81037b2eaa0e62b4d0de1
Henrique de Andrade <henriquedeandrade@gmail.com>
2021-09-13 22:51:53 +0100

    redirect pra live

---
+++ live.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    
+  <title>Conquiste Sua Vaga! Participe da Live!</title>
+    
+  <meta http-equiv="refresh" content="1; url = 'https://us02web.zoom.us/j/8407271863'" />
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+    
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para o Telegram!</p>
+
+  <p>Caso a página não carregue automaticamente, <a href="https://us02web.zoom.us/j/8407271863">clique aqui para entrar na sala do Zoom da Live</a>!</p>
+         
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");
+?> 
+
+</body>
+</html>
