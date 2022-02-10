cfb2e5f202d97136aa92d7084a72bede857b8c79
Henrique de Andrade <henrique@Henriques-MacBook-Air.local>
2022-02-10 12:01:36 -0300

    forward para elleve

---
+++ elleve.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Faça parcelamento pelo boleto na Elleve</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://bit.ly/3rujgKa'" />
+    
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+    
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para a Elleve!</p>
+
+  <p>Caso a página não carregue automaticamente, <a
+      href="https://bit.ly/3rujgKa">clique
+      aqui para entrar</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");
+?> 
+
+</body>
+</html>
