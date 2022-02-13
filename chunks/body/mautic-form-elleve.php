d34fdc4c06c61273ed45f332dba2a9912a6fca96
Henrique de Andrade <henrique@Henriques-Air.box>
2022-02-13 17:01:17 -0300

    novo forward elleve e obrigado

---
+++ chunks/body/mautic-form-elleve.php
@@ -0,0 +1,70 @@
+<div id="mauticform_wrapper_12confirmacaoelleve" class="mauticform_wrapper">
+    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=40" id="mauticform_12confirmacaoelleve" data-mautic-form="12confirmacaoelleve" enctype="multipart/form-data">
+        <div class="mauticform-error" id="mauticform_12confirmacaoelleve_error"></div>
+        <div class="mauticform-message" id="mauticform_12confirmacaoelleve_message"></div>
+        <div class="mauticform-innerform">
+
+            
+          <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
+
+            <div id="mauticform_12confirmacaoelleve_qual_o_nome_do_responsave" data-validate="qual_o_nome_do_responsave" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
+                <label id="mauticform_label_12confirmacaoelleve_qual_o_nome_do_responsave" for="mauticform_input_12confirmacaoelleve_qual_o_nome_do_responsave" class="mauticform-label">Qual o nome do responsável financeiro cadastrado na Elleve?</label>
+                <input id="mauticform_input_12confirmacaoelleve_qual_o_nome_do_responsave" name="mauticform[qual_o_nome_do_responsave]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;">Por favor, informe seu nome</span>
+            </div>
+
+            <div id="mauticform_12confirmacaoelleve_qual_o_email_cadastrado_n" data-validate="qual_o_email_cadastrado_n" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-2 mauticform-required">
+                <label id="mauticform_label_12confirmacaoelleve_qual_o_email_cadastrado_n" for="mauticform_input_12confirmacaoelleve_qual_o_email_cadastrado_n" class="mauticform-label">Qual o e-mail cadastrado na Elleve?</label>
+                <input id="mauticform_input_12confirmacaoelleve_qual_o_email_cadastrado_n" name="mauticform[qual_o_email_cadastrado_n]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;">Por favor, informe seu melhor e-mail</span>
+            </div>
+
+            <div id="mauticform_12confirmacaoelleve_quem_ira_participar_da_me1" class="mauticform-row mauticform-radiogrp mauticform-field-3">
+                <label class="mauticform-label" for="mauticform_radiogrp_radio_quem_ira_participar_da_me1_Oresponsavelfinanceiro1">Quem irá participar da mentoria Conquiste Sua Vaga?</label>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[quem_ira_participar_da_me1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_quem_ira_participar_da_me1_omesmo0" type="radio" value="omesmo">
+                    <label id="mauticform_radiogrp_label_quem_ira_participar_da_me1_omesmo0" for="mauticform_radiogrp_radio_quem_ira_participar_da_me1_omesmo0" class="mauticform-radiogrp-label">O responsável financeiro</label>
+                </div>
+                <div class="mauticform-radiogrp-row">                    <input name="mauticform[quem_ira_participar_da_me1]" class="mauticform-radiogrp-radio" id="mauticform_radiogrp_radio_quem_ira_participar_da_me1_outrapessoa1" type="radio" value="outrapessoa">
+                    <label id="mauticform_radiogrp_label_quem_ira_participar_da_me1_outrapessoa1" for="mauticform_radiogrp_radio_quem_ira_participar_da_me1_outrapessoa1" class="mauticform-radiogrp-label">Outra pessoa</label>
+                </div>
+                <span class="mauticform-errormsg" style="display: none;"></span>
+            </div>
+
+            <div id="mauticform_12confirmacaoelleve_caso_o_mentorado_seja_dif" class="mauticform-row mauticform-text mauticform-field-4">
+                <label id="mauticform_label_12confirmacaoelleve_caso_o_mentorado_seja_dif" for="mauticform_input_12confirmacaoelleve_caso_o_mentorado_seja_dif" class="mauticform-label">Caso o mentorado seja diferente do responsável financeiro, informe aqui o nome completo do mentorado</label>
+                <input id="mauticform_input_12confirmacaoelleve_caso_o_mentorado_seja_dif" name="mauticform[caso_o_mentorado_seja_dif]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;"></span>
+            </div>
+
+            <div id="mauticform_12confirmacaoelleve_caso_o_mentorado_seja_dif1" class="mauticform-row mauticform-email mauticform-field-5">
+                <label id="mauticform_label_12confirmacaoelleve_caso_o_mentorado_seja_dif1" for="mauticform_input_12confirmacaoelleve_caso_o_mentorado_seja_dif1" class="mauticform-label">Caso o mentorado seja diferente do responsável financeiro, informe aqui o e-mail do mentorado</label>
+                <input id="mauticform_input_12confirmacaoelleve_caso_o_mentorado_seja_dif1" name="mauticform[caso_o_mentorado_seja_dif1]" value="" class="mauticform-input" type="email">
+                <span class="mauticform-errormsg" style="display: none;"></span>
+            </div>
+
+            <div id="mauticform_12confirmacaoelleve_caso_o_mentorado_seja_dif2" class="mauticform-row mauticform-text mauticform-field-6">
+                <label id="mauticform_label_12confirmacaoelleve_caso_o_mentorado_seja_dif2" for="mauticform_input_12confirmacaoelleve_caso_o_mentorado_seja_dif2" class="mauticform-label">Caso o mentorado seja diferente do responsável financeiro, informe aqui o telefone do mentorado</label>
+                <input id="mauticform_input_12confirmacaoelleve_caso_o_mentorado_seja_dif2" name="mauticform[caso_o_mentorado_seja_dif2]" value="" class="mauticform-input" type="text">
+                <span class="mauticform-errormsg" style="display: none;"></span>
+            </div>
+
+            <div id="mauticform_12confirmacaoelleve_eu_li_e_concordo_com_os_a" data-validate="eu_li_e_concordo_com_os_a" data-validation-type="checkboxgrp" class="mauticform-row mauticform-checkboxgrp mauticform-field-7 mauticform-required">
+                <label class="mauticform-label" for="mauticform_checkboxgrp_checkbox_eu_li_e_concordo_com_os_a_Euconcordo1">Eu li e concordo com os <a href="https://www.conquistandosuavaga.com/termos-de-servico" target="_blank">Termos de Serviço da Mentoria Conquiste Sua Vaga</a></label>
+                <div class="mauticform-checkboxgrp-row">                    <input class="mauticform-checkboxgrp-checkbox" name="mauticform[eu_li_e_concordo_com_os_a][]" id="mauticform_checkboxgrp_checkbox_eu_li_e_concordo_com_os_a_concordo0" type="checkbox" value="concordo">
+                    <label id="mauticform_checkboxgrp_label_eu_li_e_concordo_com_os_a_concordo0" for="mauticform_checkboxgrp_checkbox_eu_li_e_concordo_com_os_a_concordo0" class="mauticform-checkboxgrp-label">Eu concordo</label>
+                </div>
+                <span class="mauticform-errormsg" style="display: none;">Isso é obrigatório.</span>
+            </div>
+
+            <div id="mauticform_12confirmacaoelleve_confirmar_a_matricula" class="mauticform-row mauticform-button-wrapper mauticform-field-8">
+                <button type="submit" name="mauticform[confirmar_a_matricula]" id="mauticform_input_12confirmacaoelleve_confirmar_a_matricula" value="" class="mauticform-button btn btn-default">Confirmar a matrícula!</button>
+            </div>
+            </div>
+        </div>
+
+        <input type="hidden" name="mauticform[formId]" id="mauticform_12confirmacaoelleve_id" value="40">
+        <input type="hidden" name="mauticform[return]" id="mauticform_12confirmacaoelleve_return" value="">
+        <input type="hidden" name="mauticform[formName]" id="mauticform_12confirmacaoelleve_name" value="12confirmacaoelleve">
+
+        </form>
+</div>
