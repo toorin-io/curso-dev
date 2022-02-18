f835be5d2ac1eb370827b336c76b46b93adbcbfc
henriquecrang <henriquedeandrade@gmail.com>
2022-02-18 13:11:06 -0300

    atualizando video de obrigado pela inscricao na live

--- obrigado-pela-inscricao-na-live.php
+++ obrigado-pela-inscricao-na-live.php
@@ -25,7 +25,7 @@ include("chunks/body/google.php");
 
 	<section class="content">
 		<div class="video">
-			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/8-xscC7UuHQ?rel=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/7QDjnxRqgb4?rel=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 	</section>
 
