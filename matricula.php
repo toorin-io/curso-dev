0d982f75e9c41bd5029c22e18767b3a30a4e39c6
henriquecrang <henriquedeandrade@gmail.com>
2022-02-28 10:47:28 -0300

    transformando páginas de contato e de matricula em redirect

--- matricula.php
+++ matricula.php
@@ -1,52 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
-      
-    <title>Conquiste Sua Vaga! Conheça a mentoria</title>
+    
+  <title>Conquiste Sua Vaga!</title>
+    
+  <meta http-equiv="refresh" content="1; url = 'https://conquistesuavaga.com/mentoria'" />
+
 
 <?php
 include("chunks/head/google.php");
 include("chunks/head/facebook.php");
-include("chunks/head/meta-mentoria.php");
+include("chunks/head/meta.php");
 ?>
-    
+ 
+
 </head>
 
-<body class="mentoria">
-
+<body class="forward">
 <?php
 include("chunks/body/google.php");
 ?>
+<div class="forward-content">
 
-<?php
-include("chunks/body/top-warning.php");
-?>
-    
-<main>
-        
-<?php
-include("chunks/body/video-mentoria.php");
-include("chunks/body/descricao-mentoria.php");
-include("chunks/body/quem-e-pedro.php");
-include("chunks/body/para-quem.php");
-include("chunks/body/calendario.php");
-include("chunks/body/depoimentos.php");
-include("chunks/body/faq.php");
-include("chunks/body/sale-box.php");
-include("chunks/body/cta-matricula-cartao.php");
-?>      
-         
-</main>
+  <p>Você está sendo redirecionado!</p>
 
-<?php
-include("chunks/body/footer.php");
-include("chunks/body/bottom-stripe.php");
+  <p>Caso a página não carregue automaticamente, <a href="https://conquistesuavaga.com/mentoria">clique aqui</a>!</p>
+
+</div>
+ <?php
 include("chunks/body/mautic-pixel.php");
 ?> 
-        
+
 </body>
-
 </html>
-
-<!-- 463 -->
