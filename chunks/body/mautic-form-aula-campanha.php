a5a11d527e95de091703f27fb7333cf0de054da4
henriquecrang <henriquedeandrade@gmail.com>
2022-03-01 22:06:47 -0300

    lançando campanha 13

--- chunks/body/mautic-form-aula-campanha.php
+++ chunks/body/mautic-form-aula-campanha.php
@@ -1,34 +1,35 @@
-<div id="mauticform_wrapper_12aulagratuita" class="mauticform_wrapper">
-    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=38" id="mauticform_12aulagratuita" data-mautic-form="12aulagratuita" enctype="multipart/form-data">
-        <div class="mauticform-error" id="mauticform_12aulagratuita_error"></div>
-        <div class="mauticform-message" id="mauticform_12aulagratuita_message"></div>
+
+<div id="mauticform_wrapper_13aulagratuita" class="mauticform_wrapper">
+    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=42" id="mauticform_13aulagratuita" data-mautic-form="13aulagratuita" enctype="multipart/form-data">
+        <div class="mauticform-error" id="mauticform_13aulagratuita_error"></div>
+        <div class="mauticform-message" id="mauticform_13aulagratuita_message"></div>
         <div class="mauticform-innerform">
 
             
           <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
 
-            <div id="mauticform_12aulagratuita_nome" data-validate="nome" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
-                <label id="mauticform_label_12aulagratuita_nome" for="mauticform_input_12aulagratuita_nome" class="mauticform-label">Nome</label>
-                <input id="mauticform_input_12aulagratuita_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
+            <div id="mauticform_13aulagratuita_nome" data-validate="nome" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
+                <label id="mauticform_label_13aulagratuita_nome" for="mauticform_input_13aulagratuita_nome" class="mauticform-label">Nome</label>
+                <input id="mauticform_input_13aulagratuita_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
                 <span class="mauticform-errormsg" style="display: none;">Isso é obrigatório.</span>
             </div>
 
-            <div id="mauticform_12aulagratuita_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-2 mauticform-required">
-                <label id="mauticform_label_12aulagratuita_email" for="mauticform_input_12aulagratuita_email" class="mauticform-label">E-mail</label>
+            <div id="mauticform_13aulagratuita_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-2 mauticform-required">
+                <label id="mauticform_label_13aulagratuita_email" for="mauticform_input_13aulagratuita_email" class="mauticform-label">E-mail</label>
                 <span class="mauticform-helpmessage">Digite aqui seu melhor e-mail</span>
-                <input id="mauticform_input_12aulagratuita_email" name="mauticform[email]" value="" class="mauticform-input" type="email">
+                <input id="mauticform_input_13aulagratuita_email" name="mauticform[email]" value="" class="mauticform-input" type="email">
                 <span class="mauticform-errormsg" style="display: none;">Por favor, informe seu melhor e-mail</span>
             </div>
 
-            <div id="mauticform_12aulagratuita_liberar_acesso" class="mauticform-row mauticform-button-wrapper mauticform-field-3">
-                <button type="submit" name="mauticform[liberar_acesso]" id="mauticform_input_12aulagratuita_liberar_acesso" value="" class="mauticform-button btn btn-default">Me envie a aula agora!</button>
+            <div id="mauticform_13aulagratuita_liberar_acesso" class="mauticform-row mauticform-button-wrapper mauticform-field-3">
+                <button type="submit" name="mauticform[liberar_acesso]" id="mauticform_input_13aulagratuita_liberar_acesso" value="" class="mauticform-button btn btn-default">Me envie a aula agora!</button>
             </div>
             </div>
         </div>
 
-        <input type="hidden" name="mauticform[formId]" id="mauticform_12aulagratuita_id" value="38">
-        <input type="hidden" name="mauticform[return]" id="mauticform_12aulagratuita_return" value="">
-        <input type="hidden" name="mauticform[formName]" id="mauticform_12aulagratuita_name" value="12aulagratuita">
+        <input type="hidden" name="mauticform[formId]" id="mauticform_13aulagratuita_id" value="42">
+        <input type="hidden" name="mauticform[return]" id="mauticform_13aulagratuita_return" value="">
+        <input type="hidden" name="mauticform[formName]" id="mauticform_13aulagratuita_name" value="13aulagratuita">
 
         </form>
 </div>
