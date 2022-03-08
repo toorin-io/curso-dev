dd4a04321011c0d75627304af76e3962fa052e0a
henriquecrang <henriquedeandrade@gmail.com>
2022-03-08 17:20:20 -0300

    corrigindo texto da p\ágina de forward do Zoom

--- live.php
+++ live.php
@@ -20,7 +20,7 @@ include("chunks/body/google.php");
 ?>
 <div class="forward-content">
 
-  <p>Você está sendo redirecionado para o Telegram!</p>
+  <p>Você está sendo redirecionado para o Zoom!</p>
 
   <p>Caso a página não carregue automaticamente, <a href="https://us02web.zoom.us/j/8407271863">clique aqui para entrar na sala do Zoom da Live</a>!</p>
          
