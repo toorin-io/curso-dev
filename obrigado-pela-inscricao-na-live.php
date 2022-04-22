cfa0962b2b49fecd054096a065b0cc012814a2a2
henriquecrang <henriquedeandrade@gmail.com>
2022-04-21 23:03:25 -0300

    Criando live para Campanha 14

--- obrigado-pela-inscricao-na-live.php
+++ obrigado-pela-inscricao-na-live.php
@@ -25,24 +25,31 @@ include("chunks/body/google.php");
 
 	<section class="content">
 		<div class="video">
-			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/b0vEndWO0jw?rel=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/n__6mE8DDl0?rel=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 	</section>
 
 	<section class="selling-points">
 		<div class="content">
+            <p class="selling-text">
+				<strong>Me adicione no LinkedIn!</strong>
+				Me envie uma mensagem se apresentando no LinkedIn para eu te adicionar!
+				<span>
+					<a target="_blank" href="linkedin"><img class="selling-icon" src="images/icons/linkedin.svg" alt=""></a>
+				</span>
+			</p>	
 			<p class="selling-text">
-				<strong>Acesse o Telegram!</strong>
-				Entre no canal Conquiste Sua Vaga e receba lá o link para participar da live!
+				<strong>Receba o link para a live!</strong>
+				Entre no canal Conquiste Sua Vaga e receba lá o link para acessar a live!
 				<span>
 					<a target="_blank" href="telegram"><img class="selling-icon" src="images/icons/telegram.png" alt=""></a>
 				</span>
 			</p>	
             		<p class="selling-text">
-				<strong>Divulgue no WhatsApp!</strong>
-				Chame seus amigos que querem virar dev no WhatsApp para participarem da live também!
+				<strong>Convide seus amigos!</strong>
+				Chame seus amigos que querem virar dev no WhatsApp para participarem da live com você!
 				<span>
-					<a target="_blank" href="whatsapp-live.php"><img class="selling-icon" src="images/icons/whatsapp.png" alt=""></a>
+					<a target="_blank" href="whatsapp-live"><img class="selling-icon" src="images/icons/whatsapp.png" alt=""></a>
 				</span>
 			</p>		
 
