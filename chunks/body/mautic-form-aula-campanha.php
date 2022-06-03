1c6817b8069e66ae1190d2da5287a35ec8f696ea
henriquecrang <henriquedeandrade@gmail.com>
2022-06-02 21:29:38 -0300

    lançando campanha 16

--- chunks/body/mautic-form-aula-campanha.php
+++ chunks/body/mautic-form-aula-campanha.php
@@ -1,35 +1,35 @@
 
-<div id="mauticform_wrapper_15aulagratuitafacebook" class="mauticform_wrapper">
-    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=53" id="mauticform_15aulagratuitafacebook" data-mautic-form="15aulagratuitafacebook" enctype="multipart/form-data">
-        <div class="mauticform-error" id="mauticform_15aulagratuitafacebook_error"></div>
-        <div class="mauticform-message" id="mauticform_15aulagratuitafacebook_message"></div>
+<div id="mauticform_wrapper_16aulagratuitafacebook" class="mauticform_wrapper">
+    <form autocomplete="false" role="form" method="post" action="https://mkt.henriqueandrade.com.br/form/submit?formId=60" id="mauticform_16aulagratuitafacebook" data-mautic-form="16aulagratuitafacebook" enctype="multipart/form-data">
+        <div class="mauticform-error" id="mauticform_16aulagratuitafacebook_error"></div>
+        <div class="mauticform-message" id="mauticform_16aulagratuitafacebook_message"></div>
         <div class="mauticform-innerform">
 
             
           <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
 
-            <div id="mauticform_15aulagratuitafacebook_nome" data-validate="nome" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
-                <label id="mauticform_label_15aulagratuitafacebook_nome" for="mauticform_input_15aulagratuitafacebook_nome" class="mauticform-label">Nome</label>
-                <input id="mauticform_input_15aulagratuitafacebook_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
+            <div id="mauticform_16aulagratuitafacebook_nome" data-validate="nome" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-1 mauticform-required">
+                <label id="mauticform_label_16aulagratuitafacebook_nome" for="mauticform_input_16aulagratuitafacebook_nome" class="mauticform-label">Nome</label>
+                <input id="mauticform_input_16aulagratuitafacebook_nome" name="mauticform[nome]" value="" class="mauticform-input" type="text">
                 <span class="mauticform-errormsg" style="display: none;">Isso é obrigatório.</span>
             </div>
 
-            <div id="mauticform_15aulagratuitafacebook_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-2 mauticform-required">
-                <label id="mauticform_label_15aulagratuitafacebook_email" for="mauticform_input_15aulagratuitafacebook_email" class="mauticform-label">E-mail</label>
+            <div id="mauticform_16aulagratuitafacebook_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-2 mauticform-required">
+                <label id="mauticform_label_16aulagratuitafacebook_email" for="mauticform_input_16aulagratuitafacebook_email" class="mauticform-label">E-mail</label>
                 <span class="mauticform-helpmessage">Digite aqui seu melhor e-mail</span>
-                <input id="mauticform_input_15aulagratuitafacebook_email" name="mauticform[email]" value="" class="mauticform-input" type="email">
+                <input id="mauticform_input_16aulagratuitafacebook_email" name="mauticform[email]" value="" class="mauticform-input" type="email">
                 <span class="mauticform-errormsg" style="display: none;">Por favor, informe seu melhor e-mail</span>
             </div>
 
-            <div id="mauticform_15aulagratuitafacebook_liberar_acesso" class="mauticform-row mauticform-button-wrapper mauticform-field-3">
-                <button type="submit" name="mauticform[liberar_acesso]" id="mauticform_input_15aulagratuitafacebook_liberar_acesso" value="" class="mauticform-button btn btn-default">Me envie a aula agora!</button>
+            <div id="mauticform_16aulagratuitafacebook_liberar_acesso" class="mauticform-row mauticform-button-wrapper mauticform-field-3">
+                <button type="submit" name="mauticform[liberar_acesso]" id="mauticform_input_16aulagratuitafacebook_liberar_acesso" value="" class="mauticform-button btn btn-default">Me envie a aula agora!</button>
             </div>
             </div>
         </div>
 
-        <input type="hidden" name="mauticform[formId]" id="mauticform_15aulagratuitafacebook_id" value="53">
-        <input type="hidden" name="mauticform[return]" id="mauticform_15aulagratuitafacebook_return" value="">
-        <input type="hidden" name="mauticform[formName]" id="mauticform_15aulagratuitafacebook_name" value="15aulagratuitafacebook">
+        <input type="hidden" name="mauticform[formId]" id="mauticform_16aulagratuitafacebook_id" value="60">
+        <input type="hidden" name="mauticform[return]" id="mauticform_16aulagratuitafacebook_return" value="">
+        <input type="hidden" name="mauticform[formName]" id="mauticform_16aulagratuitafacebook_name" value="16aulagratuitafacebook">
 
         </form>
 </div>
