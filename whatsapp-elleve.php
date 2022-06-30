44ff1877e2c225aec88ab9b6e5485cb3aab91423
henriquecrang <henriquedeandrade@gmail.com>
2022-06-30 19:09:25 -0300

    adicionando novas mensagens de contato

---
+++ whatsapp-elleve.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Converse comigo pelo WhatsApp</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://api.whatsapp.com/send?phone=5521977386162&text=Olá%20Pedro!%20Eu%20acabei%20de%20conversar%20com%20o%20Chatbot%20do%20boleto!'" />
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para o WhatsApp!</p>
+
+  <p>Caso a página não carregue automaticamente, <a
+      href="https://api.whatsapp.com/send?phone=5521977386162&text=Olá%20Pedro!%20Eu%20acabei%20de%20conversar%20com%20o%20Chatbot%20do%20boleto!">clique
+      aqui para me enviar a mensagem</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");?> 
+
+</body>
+</html>
