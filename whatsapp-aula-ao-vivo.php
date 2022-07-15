0bdaa9cba7c3b3d4d9385db3dcb84435c51af068
henriquecrang <henriquedeandrade@gmail.com>
2022-07-14 21:51:13 -0300

    criando mensagem direta da aula

---
+++ whatsapp-aula-ao-vivo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Converse comigo pelo WhatsApp</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://api.whatsapp.com/send?phone=5521977386162&text=Olá%20Pedro!%20Eu%20acabei%20de%20assistir%20a%20sua%20aula%20e%20quero%20conversar%20sobre%20a%20mentoria%20Conquiste%20Sua%20Vaga!'" />
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para o WhatsApp!</p>
+
+  <p>Caso a página não carregue automaticamente, <a
+      href="https://api.whatsapp.com/send?phone=5521977386162&text=Olá%20Pedro!%20Eu%20acabei%20de%20assistir%20a%20sua%20aula%20e%20quero%20conversar%20sobre%20a%20mentoria%20Conquiste%20Sua%20Vaga!">clique
+      aqui para me enviar a mensagem</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");?> 
+
+</body>
+</html>
