6ecac125d043d0247e109167f25f5c7558d14b1c
henriquecrang <henriquedeandrade@gmail.com>
2022-07-30 21:53:33 -0300

    migrando forms para AC

--- confirmacao-matricula.php
+++ confirmacao-matricula.php
@@ -1,79 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-    <head>
+<head>
+  <title>Conquiste Sua Vaga! Confirmação de matrícula</title>
 
-        
-        <title>Conquiste Sua Vaga - acesso à comunidade</title>
-
-
+    <meta http-equiv="refresh" content="1; url = 'https://conquistandosuavaga.activehosted.com/f/6'" />
 <?php
 include("chunks/head/google.php");
 include("chunks/head/facebook.php");
-include("chunks/head/meta-mentoria.php");
+include("chunks/head/meta.php");
 ?>
-        
-            
-<?php
-include("chunks/head/mautic-form.php");
-?>
+ 
 
-                
-<style type="text/css" scoped>
-    .mauticform_wrapper { max-width: 600px; margin: 10px auto; color: #6f6f6f; font-weight: 400; font-family: 'Source Sans Pro', 'Helvetica Neue', 'Arial', 'sans-serif';font-size: 18px;}
-    .mauticform-innerform {}
-    .mauticform-post-success {}
-    .mauticform-name { font-weight: bold; font-size: 1.5em; margin-bottom: 3px; }
-    .mauticform-description { margin-top: 2px; margin-bottom: 10px; }
-    .mauticform-error { margin-bottom: 10px; color: red; }
-    .mauticform-message { margin-bottom: 10px;color: green; }
-    .mauticform-row { display: block; margin-bottom: 20px; }
-    .mauticform-label { font-size: 1.1em; display: block; font-weight: bold; margin-bottom: 5px; color: #21c5ba; }
-    .mauticform-row.mauticform-required .mauticform-label:after { color: #e32; content: " *"; display: inline; }
-    .mauticform-helpmessage { display: block; font-size: 0.9em; margin-bottom: 3px; }
-    .mauticform-errormsg { display: block; color: red; margin-top: 2px; }
-    .mauticform-selectbox, .mauticform-input, .mauticform-textarea { width: 100%; padding: 0.5em 0.5em; border: 0; border-bottom: 1px solid #21c5ba; background: #fff; box-sizing: border-box; }
-    .mauticform-checkboxgrp-row {}
-    .mauticform-checkboxgrp-label { font-weight: normal; }
-    .mauticform-checkboxgrp-checkbox {}
-    .mauticform-radiogrp-row {}
-    .mauticform-radiogrp-label { font-weight: normal; }
-    .mauticform-radiogrp-radio {}
-    .mauticform-button-wrapper .mauticform-button.btn-default { color: #21c5ba;background-color: #ffffff;border-color: #21c5ba;}
-    .mauticform-button-wrapper .mauticform-button { display: inline-block;margin-bottom: 0;font-weight: 600;text-align: center;vertical-align: middle;cursor: pointer;background-image: none;border: 1px solid transparent;white-space: nowrap;padding: 6px 12px;font-size: 18px;line-height: 1.3856;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}
-    .mauticform-button-wrapper .mauticform-button.btn-default[disabled] { background-color: #ffffff; border-color: #dddddd;}
-</style>
-        
+</head>
 
-    </head>
-    <body>
-
+<body class="forward">
 <?php
 include("chunks/body/google.php");
 ?>
+<div class="forward-content">
 
-        <header class="content header" style="margin-bottom:0em">
-            
-            <h1 class="big-title" style="font-size:3.5em; margin-bottom:0.5em" >Conquiste Sua Vaga!</h1>
-        </header>
+  <p>Você está sendo redirecionado para o formulário de confirmação de matrícula!</p>
 
-        <h2 style="max-width: 970px; margin: 0 auto; text-align: center; font-weight: 300; margin-bottom:1em">
-Para ter acesso à comunidade exclusiva Conquiste Sua Vaga no Discord você precisa responder a este rápido questionário!</h2>
+  <p>Caso a página não carregue automaticamente, <a href="https://conquistandosuavaga.activehosted.com/f/6">clique aqui para acessar </a>!</p>
 
-
-        
+</div>
 <?php
-include("chunks/body/mautic-form-confirmacao-matricula.php");
-?>
-
-
-
-<?php
-include("chunks/body/footer.php");
-include("chunks/body/top-stripe.php");
-include("chunks/body/bottom-stripe.php");
 include("chunks/body/mautic-pixel.php");
 ?> 
-    
-        
-        </body>
+
+</body>
+
 </html>
