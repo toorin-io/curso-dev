1e3b0115acf104f24a25f44164aceed7d06963c4
henriquecrang <henriquedeandrade@gmail.com>
2022-09-25 08:28:00 -0300

    atualizando aula

--- chunks/body/video-aula.php
+++ chunks/body/video-aula.php
@@ -1,3 +1,3 @@
 		<div class="video">
-			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/t8cV4jW5jaM?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/a_otB9CJsr8?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
