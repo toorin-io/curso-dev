0dc42c59a7aef7ada23d3d5471c14b519105b0fe
henriquecrang <henriquedeandrade@gmail.com>
2022-10-01 12:51:15 -0300

    removendo pixel do meta ads para ser tratado pelo Google Tag Manager

--- chunks/head/facebook.php
+++ chunks/head/facebook.php
@@ -1,21 +1,5 @@
-    <!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '443908680129622');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=443908680129622&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Facebook Pixel Code -->
 
+<!-- aqui ficava o pixel do Meta Ads. Tiramos e ele agora é tratado pelo Google Tag Manager -->
 
 <!-- validadr de domínio -->
 <meta name="facebook-domain-verification" content="xfjonj9btrtddzst037c89esolmq4v" />
