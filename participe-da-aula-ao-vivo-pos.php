10cd75592203beccc647da7d6b349251299a89b9
henriquecrang <henriquedeandrade@gmail.com>
2022-10-13 09:40:44 -0300

    Nova estrutura para aula ao vivo fora do ar

---
+++ participe-da-aula-ao-vivo-pos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Assista à aula</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://youtu.be/iygtb_nmyd0?t=424'" />
+    
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+    
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para o YouTube!</p>
+
+  <p>Caso a gravação da aula não carregue automaticamente, <a
+      href="https://youtu.be/iygtb_nmyd0?t=424">clique
+      aqui para assistir a aula</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");
+?> 
+
+</body>
+</html>
