43b437c3a4d8a5ff12f9b8d3b84ebd50a5c3ff89
henriquecrang <github@henriqueandrade.com.br>
2022-12-02 13:39:59 -0300

    aula ao vivo 22

--- obrigado-aula-ao-vivo.php
+++ obrigado-aula-ao-vivo.php
@@ -32,17 +32,19 @@ include("chunks/body/google.php");
 	<section class="selling-points">
 		<div class="content">
             <p class="selling-text">
-				<strong>Adicione na sua agenda!</strong>
-				Não esqueça da aula! Adicione um lembrete na sua agenda!
+				
+						
+                                <strong>	<a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=O+que+voc%C3%AA+deve+fazer+hoje+para+virar+dev+ainda+em+2022%21+-+Aula+ao+vivo&details=Nesta+segunda-feira+%C3%A0s+18h30+eu+e+o+Henrique+de+Andrade+vamos+te+explicar+O+que+voc%C3%AA+deve+fazer+hoje+para+virar+dev+ainda+em+2022%21&location=https%3A%2F%2Fus02web.zoom.us%2Fj%2F88094942874%3Fpwd%3DelpqSExqS3Qyb2lxTDhoOEJZS3FCQT09&dates=20221107T213000Z%2F20221107T230000Z">Adicione na sua agenda!</a></strong>
+                <a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=O+que+voc%C3%AA+deve+fazer+hoje+para+virar+dev+ainda+em+2022%21+-+Aula+ao+vivo&details=Nesta+segunda-feira+%C3%A0s+18h30+eu+e+o+Henrique+de+Andrade+vamos+te+explicar+O+que+voc%C3%AA+deve+fazer+hoje+para+virar+dev+ainda+em+2022%21&location=https%3A%2F%2Fus02web.zoom.us%2Fj%2F88094942874%3Fpwd%3DelpqSExqS3Qyb2lxTDhoOEJZS3FCQT09&dates=20221107T213000Z%2F20221107T230000Z">         Não esqueça da aula! Adicione um lembrete na sua agenda! </a>
 				<span>
-					<a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=O+que+voc%C3%AA+deve+fazer+hoje+para+virar+dev+ainda+em+2022%21+-+Aula+ao+vivo&details=Nesta+segunda-feira+%C3%A0s+18h30+eu+e+o+Henrique+de+Andrade+vamos+te+explicar+O+que+voc%C3%AA+deve+fazer+hoje+para+virar+dev+ainda+em+2022%21&location=https%3A%2F%2Fus02web.zoom.us%2Fj%2F88094942874%3Fpwd%3DelpqSExqS3Qyb2lxTDhoOEJZS3FCQT09&dates=20221107T213000Z%2F20221107T230000Z"><img class="selling-icon" src="images/icons/calendar-icon.png" alt=""></a>
+			<a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=Como+fazer+transi%C3%A7%C3%A3o+de+carreira+e+ser+contratado+como+dev+em+poucos+meses%21&details=Nesta+segunda-feira+%C3%A0s+18h30+eu+e+o+Henrique+de+Andrade+vamos+te+explicar+como+fazer+transi%C3%A7%C3%A3o+de+carreira+e+ser+contratado+como+dev+em+poucos+meses%21&location=https%3A%2F%2Fus02web.zoom.us%2Fj%2F88060206680%3Fpwd%3DUlhDMFZrZ1I4MkliUDBCaVFQQUNtZz09&dates=20221206T213000Z%2F20221207T000000Z"><img class="selling-icon" src="images/icons/calendar-icon.png" alt=""></a>
 				</span>
 			</p>	
 			<p class="selling-text">
-				<strong>Conheça o meu método!</strong>
-				Assista agora à aula com os 3 passos para começar sua carreira como dev!
-				<span>
-					<a target="_blank" href="https://www.conquistesuavaga.com/aula"><img class="selling-icon" src="images/icons/speaking.png" alt=""></a>
+					
+                <strong><a target="_blank" href="https://www.conquistesuavaga.com/aula">Conheça o meu método!</a></strong>
+                <a target="_blank" href="https://www.conquistesuavaga.com/aula">		Assista agora à aula com os 3 passos para começar sua carreira como dev! </a>
+				<span><a target="_blank" href="https://www.conquistesuavaga.com/aula"><img class="selling-icon" src="images/icons/speaking.png" alt=""></a>
 				</span>
 			</p>		
 
