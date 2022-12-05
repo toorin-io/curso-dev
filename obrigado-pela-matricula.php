d51b64fc4e33fdbbf867e12001e583b75acf1196
henriquecrang <github@henriqueandrade.com.br>
2022-12-05 12:54:44 -0300

    atualizando videos

--- obrigado-pela-matricula.php
+++ obrigado-pela-matricula.php
@@ -26,7 +26,7 @@ include("chunks/body/google.php");
 
 	<section class="content">
 		<div class="video">
-			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/eCLem-vqyt8?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/94ZgHxnN9bI?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 	</section>
 
