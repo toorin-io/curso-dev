7cd0b8c6e40ee8c2b987fe0ad14c6d10b4f57dca
henriquecrang <github@henriqueandrade.com.br>
2022-12-28 13:31:34 -0300

    atualizando aula Lançamento 23

--- chunks/body/video-aula.php
+++ chunks/body/video-aula.php
@@ -1,3 +1,3 @@
 		<div class="video">
-			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/85jyenjkmSA?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="100%" height="550px" src="https://www.youtube.com/embed/qPDWyIgzWc4?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
