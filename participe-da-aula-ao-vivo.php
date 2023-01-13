ca9016e1055353928f94958fe9dce71daf52d1ca
henriquecrang <github@henriqueandrade.com.br>
2023-01-13 17:51:53 -0300

    Criando página para inscrição na Live

---
+++ participe-da-aula-ao-vivo.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+
+        
+        <title>Conquiste Sua Vaga - Participe da Aula ao Vivo</title>
+
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta-aula.php");
+?>
+        
+            
+<?php
+include("chunks/head/mautic-form.php");
+?>
+
+
+    </head>
+    <body>
+
+<?php
+include("chunks/body/google.php");
+?>
+
+        <header class="content header">
+            <h1 class="big-title">Aula ao vivo com Pedro Marins!</h1>
+            <h2>Participe gratuitamente da aula ao vivo <b style="color:red">O que você deve fazer em janeiro para virar dev em 2023
+!</b> na segunda-feira às 18h30!</h2>
+        </header>
+
+<?php
+include("chunks/body/ac-inscricao-ao-vivo.php");
+?>
+
+
+
+<?php
+include("chunks/body/footer.php");
+include("chunks/body/top-stripe.php");
+include("chunks/body/bottom-stripe.php");
+include("chunks/body/mautic-pixel.php");
+?> 
+    
+        
+        </body>
+</html>
