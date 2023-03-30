61cdb747ed8051cdf7ef39c9bf2b71aefd394417
henriquecrang <github@henriqueandrade.com.br>
2023-03-30 10:45:38 -0300

    Trazendo para cá infra de entrega de projetos

---
+++ entrega-projeto-desafiador.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Conquiste Sua Vaga! Entrega do Projeto Desafiador</title>
+
+    <meta http-equiv="refresh"
+    content="1; url = 'https://forms.gle/JiXgKxf8kkicJnbh7'" />
+
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta.php");
+?>
+
+</head>
+
+<body class="forward">
+<?php
+include("chunks/body/google.php");
+?>
+<div class="forward-content">
+
+  <p>Você está sendo redirecionado para o formulário de entrega do projeto!</p>
+
+  <p>Caso a página não carregue automaticamente, <a
+      href="https://forms.gle/JiXgKxf8kkicJnbh7">clique
+      aqui para acessar</a>!</p>
+</div>
+<?php
+include("chunks/body/mautic-pixel.php");?> 
+
+</body>
+</html>
