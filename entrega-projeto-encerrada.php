34c64be694ccb2f14e5c15e51adf247ebe3b305a
henriquecrang <github@henriqueandrade.com.br>
2023-03-30 10:55:36 -0300

    Criando página para entrega de projetos encerrada

---
+++ entrega-projeto-encerrada.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    
+	<title>Conquiste Sua Vaga! Parabéns pela matrícula!</title>
+
+    
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta-mentoria.php");
+?>
+    
+    </head>
+
+<body>
+    
+<?php
+include("chunks/body/google.php");
+?>
+
+	<header class="content header">
+		<h1>Entrega de projetos encerrada!</h1>
+	</header>
+
+	<section class="content">
+		<div class="video">
+			<iframe width="100%" height="550px" src="https://player.vimeo.com/video/762991400?h=fc0133d52a&amp" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</section>
+
+	<div class="top-stripe"></div>
+
+    <?php
+include("chunks/body/footer.php");
+include("chunks/body/top-stripe.php");
+include("chunks/body/bottom-stripe.php");
+include("chunks/body/mautic-pixel.php");
+?>
+    
+    </body>
+
+</html>
