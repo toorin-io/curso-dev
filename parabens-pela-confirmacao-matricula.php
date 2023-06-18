f66fe0dd9d2051419fb999fc9b86274be5c49dbd
henriquecrang <github@henriqueandrade.com.br>
2023-06-18 19:59:34 -0300

    Recriando página

---
+++ parabens-pela-confirmacao-matricula.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+	<title>Siga a instrução abaixo para acessar imediatamente a imersão!</title>
+	  
+<?php
+include("chunks/head/google.php");
+include("chunks/head/facebook.php");
+include("chunks/head/meta-mentoria.php");
+
+    ?>
+    
+    </head>
+
+<body>
+    
+<?php
+include("chunks/body/google.php");
+?>
+
+	<header class="content header">
+		<h1>Siga a instrução abaixo para garantir que você terá acesso à Imersão Vire Dev!</h1>
+	</header>
+
+
+	<section class="selling-points">
+		<div class="content">
+			<p class="selling-text">
+                <strong><a class="selling-text-a" target="_blank" href="https://www.circle.co">Clique aqui para entrar na área de membros da Imersão Vire Dev!</a></strong>
+			</p>			
+
+		</div>
+	</section>
+
+
+<?php
+include("chunks/body/footer.php");
+include("chunks/body/top-stripe.php");
+include("chunks/body/bottom-stripe.php");
+include("chunks/body/mautic-pixel.php");
+?>
+
+    
+    </body>
+
+</html>
