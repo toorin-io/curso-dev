6bff1104d8008f4edd361c1c4417f29802698206
geovanarochamp <geovana.rocha.martins@gmail.com>
2023-06-19 05:58:14 -0300

    feat: botao-cta-com-temporizador

---
+++ chunks/body/vire-dev/botao-cta-aula-com-temporizador.php
@@ -0,0 +1,11 @@
+<a target="_blank" href="/imersao.php" id="cta-aula" class="cta-button">
+	Quero dar o próximo passo!
+</a>
+
+<script>
+setTimeout( function() {
+  document.getElementById("cta-aula").style.display = "flex"
+}, 3000)
+  
+</script>
+
