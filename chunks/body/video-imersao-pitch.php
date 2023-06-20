eac6d5287e6359103ef649267270f0ab116bce45
henriquecrang <github@henriqueandrade.com.br>
2023-06-20 10:03:19 -0300

    Página focada na imersão

--- chunks/body/video-imersao-pitch.php
+++
@@ -1,5 +0,0 @@
-        <section class="content">
-            <div class="video">
-                <iframe width="100%" height="550px" src="https://www.youtube.com/embed/V16WzSFdQ-8?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </section>
---
+++ chunks/body/vire-dev/chamada-com-video-pitch.php
@@ -0,0 +1,21 @@
+        <main class="hero">
+  <div class="hero-overlay">
+    <p class="hero-text text-gray-400">
+      MAIS DE 1.000 ALUNOS JÁ VIRARAM DEV COM O MÉTODO DA IMERSÃO
+    </p>
+    <h1 class="headline text-white">VIRE DEV</h1>
+    <h4 class="subheadline text-gray-200">
+      O método que você precisa para se tornar o dev desejado pelo mercado!
+    </h4>
+            <div class="video">
+                <iframe width="60%" height="550px" src="https://www.youtube.com/embed/2c4c85xP_A0?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+      </br></br></br>
+            <?php
+include("vire-dev/botao-matricula.php");
+?>
+
+  </div>
+</main>
+
