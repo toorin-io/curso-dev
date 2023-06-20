3be1038bd10cd849b44178d221764bc13f0d017e
henriquecrang <github@henriqueandrade.com.br>
2023-06-20 18:59:36 -0300

    Criando nova página de mensagem pro mwhatsapp

---
+++ whatsapp-vip.php
@@ -0,0 +1,6 @@
+<?php
+$link = "https://api.whatsapp.com/send?phone=5521977386162&text=Eu%20faço%20parte%20da%20rede%20do%20Pedro%20e%20quero%20participar%20da%20Imersão%20Vire%20Dev%20com%2070%%20 de%20desconto!";
+include("redirect.php");
+?>
+
+
