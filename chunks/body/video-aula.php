f109e2ec41ffe367136751ec96f4279a041befd8
Jordy Alcides <jordy.alcides@gmail.com>
2023-06-22 17:23:42 -0300

    Melhorando responsividade da página video-aula;

--- chunks/body/video-aula.php
+++ chunks/body/video-aula.php
@@ -1,3 +1,3 @@
 		<div class="video-box">
-			<iframe width="100%" height="600px" src="https://www.youtube.com/embed/vPkQFjSm16U?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="100%" src="https://www.youtube.com/embed/vPkQFjSm16U?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
