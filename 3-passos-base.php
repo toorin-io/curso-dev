3ba78d34f389bb22300d663c3ca50a6242db78bc
Jordy Alcides <jordy.alcides@gmail.com>
2023-06-23 14:23:05 -0300

    Movendo captura de tags para 3-passos-base;

--- 3-passos-base.php
+++ 3-passos-base.php
@@ -1,3 +1,10 @@
+<?php
+$utm_source = isset($_GET['utm_source'])?$_GET['utm_source'] : null;
+$utm_medium = isset($_GET['utm_medium'])?$_GET['utm_medium'] : null;
+$utm_campaign = isset($_GET['utm_campaign'])?$_GET['utm_campaign'] : null;
+$utm_term = isset($_GET['utm_term'])?$_GET['utm_term'] : null;
+$utm_content = isset($_GET['utm_content'])?$_GET['utm_content'] : null;
+?>
 <html lang="pt-BR">
 	<head>
     <meta charset="UTF-8" />
@@ -860,9 +867,7 @@
           >
             <path
               class="elementor-shape-fill"
-              d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
-	c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
-	c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"
+              d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z" 
             />
           </svg>
         </div>
