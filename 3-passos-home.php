060bc8910d3d4cc01679a796bdc796140ce73016
henriquecrang <github@henriqueandrade.com.br>
2023-06-23 11:07:08 -0300

    Removendo lixo de rascunho que ficou em páginas de captura

--- 3-passos-home.php
+++ 3-passos-home.php
@@ -6,8 +6,3 @@ $utm_term = isset($_GET['utm_term'])?$_GET['utm_term'] : null;
 $utm_content = isset($_GET['utm_content'])?$_GET['utm_content'] : null;
 include("3-passos-base.php");
 ?>
-f
-h
-instagram
-linkedin
-q
--- 3-passos-linkedin.php
+++ 3-passos-linkedin.php
@@ -1,8 +1,3 @@
 <?php
 include("3-passos-base.php");
 ?>
-f
-h
-instagram
-linkedin
-q
