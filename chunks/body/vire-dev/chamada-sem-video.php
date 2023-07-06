1dde46a665f7d102fc6c9ec01b07f2d0899f0357
Jordy Alcides <jordy.alcides@gmail.com>
2023-07-06 14:57:32 -0300

    Fixing styles for hero image on page imersao;

--- chunks/body/vire-dev/chamada-sem-video.php
+++ chunks/body/vire-dev/chamada-sem-video.php
@@ -1,7 +1,7 @@
 <main class="hero hero-dark-blue">
   <div class="hero-overlay content hero-wrapper">
-  
-    <div>      
+
+    <div>
       <h1 class="conheca-headline text-white">VIRE DEV</h1>
       <h4 class="conheca-subheadline text-gray-400">
         O método que você precisa para se tornar o dev desejado pelo mercado!
@@ -13,16 +13,15 @@
       <div class="conheca-text-icon">
         <i class="fas fa-check text-primary"></i>
         <p class="conheca-text text-gray-400">
-          Validado por mais de 1.000 alunos 
+          Validado por mais de 1.000 alunos
         </p>
       </div>
     </div>
 
     <div>
-        <img src="images/pedromarins-transparent.png">
-    </div>    
+      <img style="width: 100%;" src="images/pedromarins-transparent.png">
+    </div>
   </div>
 
 
 </main>
-
